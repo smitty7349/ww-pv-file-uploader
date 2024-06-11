0374041514e6986e327231ae1b7a5b7e25d1d89d
Chase Macbook Air <chadonsom@gmail.com>
2024-06-10 20:19:27 -0400

    add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+git checkout master
+git pull
+git merge dev
+
+# Ensure the argument is provided
+if [ -z "$1" ]; then
+  echo "Error: No version type (patch, minor, major) provided."
+  exit 1
+fi
+
+# Update the version using npm
+npm version $1
+
+# Get the new tag created by npm version
+TAG=$(git describe --tags)
+
+# Push the commit and the tag together
+git push --follow-tags
+
+git checkout dev
+
+PACKAGE_VERSION=$(node -p "require('./package.json').version")
+echo "Version $TAG (package: $PACKAGE_VERSION) has been deployed to master and dev branches."
