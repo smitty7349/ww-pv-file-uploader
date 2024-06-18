f82f297c6acc2e32359d330fa08931b094c37067
Chase Macbook Air <chadonsom@gmail.com>
2024-06-17 21:39:19 -0400

    add editor bindings for style, and for array of files

--- ww-config.js
+++ ww-config.js
@@ -27,6 +27,69 @@ export default {
       bindable: true,
       responsive: true,
     },
+    style: {
+      type: "TextSelect",
+      label: {
+        en: "Style",
+      },
+      options: {
+        options: [
+          { value: "basic", default: true, label: { en: "Basic" }, icon: "" },
+          { value: "inline", label: { en: "Inline" }, icon: "" },
+          { value: "minimal", label: { en: "Minimal" }, icon: "" },
+        ],
+      },
+    },
+    uploadedFiles: {
+      label: {
+        en: "Uploaded Files",
+      },
+      type: "Array",
+      bindable: true,
+      responsive: true,
+      options: {
+        fixed: false,
+        item: {
+          type: "Object",
+          options: {
+            item: {
+              uuid: {
+                label: {
+                  en: "uuid",
+                },
+                type: "Text",
+                bindable: true,
+                responsive: true,
+              },
+              cdnUrl: {
+                label: {
+                  en: "cdnUrl",
+                },
+                type: "Text",
+                bindable: true,
+                responsive: true,
+              },
+              name: {
+                label: {
+                  en: "fileName",
+                },
+                type: "Text",
+                bindable: true,
+                responsive: true,
+              },
+              size: {
+                label: {
+                  en: "size",
+                },
+                type: "Number",
+                bindable: true,
+                responsive: true,
+              },
+            },
+          },
+        },
+      },
+    },
     enableScaleCrop: {
       label: {
         en: "Enable Scale Crop",
